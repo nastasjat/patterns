6dc14131f7ec7a5dc6d90d17bdcf4342f00428f7
Anastasiia Trakalo <nastyatrakalo@gmail.com>
2024-05-30 01:52:01 +0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'patterns',
+  webDir: 'www'
+};
+
+export default config;
